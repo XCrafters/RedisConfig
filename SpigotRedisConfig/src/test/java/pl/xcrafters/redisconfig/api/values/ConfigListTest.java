529eeb2b228681f34749981bfa1875f403ced82f
defabricated <defabricated@gmail.com>
2019-11-17 12:09:15 +0100

    Update SpigotRedisConfig test

--- SpigotRedisConfig/src/test/java/pl/xcrafters/redisconfig/api/values/ConfigListTest.java
+++ SpigotRedisConfig/src/test/java/pl/xcrafters/redisconfig/api/values/ConfigListTest.java
@@ -2,14 +2,9 @@ package pl.xcrafters.redisconfig.api.values;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.Server;
-import org.bukkit.plugin.PluginBase;
-import org.bukkit.plugin.PluginManager;
-import org.bukkit.plugin.java.JavaPlugin;
 import org.junit.Test;
 import pl.xcrafters.redisconfig.api.FieldType;
 import pl.xcrafters.spigotredisconfig.SpigotRedisConfigPlugin;
-import pl.xcrafters.spigotredisconfig.api.RedisConfigAPI;
 import pl.xcrafters.spigotredisconfig.api.SpigotRedisConfigManager;
 import pl.xcrafters.spigotredisconfig.api.values.SpigotConfigColor;
 import pl.xcrafters.spigotredisconfig.api.values.SpigotConfigMaterial;
@@ -17,8 +12,6 @@ import pl.xcrafters.spigotredisconfig.api.values.SpigotConfigMaterial;
 import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class ConfigListTest {
 
@@ -36,7 +29,7 @@ public class ConfigListTest {
         ConfigList<SpigotConfigColor> colorsDeserialized = configManager.getSpigotColorList("colors");
         assertEquals(colors, colorsDeserialized);
 
-        ConfigList<SpigotConfigMaterial> materials = new ConfigList<SpigotConfigMaterial>(Arrays.asList(new SpigotConfigMaterial(Material.ACACIA_DOOR), new SpigotConfigMaterial(Material.ACACIA_DOOR_ITEM), new SpigotConfigMaterial(Material.ACACIA_FENCE)));
+        ConfigList<SpigotConfigMaterial> materials = new ConfigList<SpigotConfigMaterial>(Arrays.asList(new SpigotConfigMaterial(Material.ACACIA_DOOR), new SpigotConfigMaterial(Material.ACACIA_BUTTON), new SpigotConfigMaterial(Material.ACACIA_FENCE)));
         configManager.set("materials", materials);
         ConfigList<SpigotConfigMaterial> materialsDeserialized = configManager.getSpigotMaterialList("materials");
         assertEquals(materials, materialsDeserialized);
